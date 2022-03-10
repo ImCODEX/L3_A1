647ebdf0bffcc5dbb4f97d5025a329a15c4862ca
rpostescu_01 <53984429+ImCODEX@users.noreply.github.com>
2022-03-10 16:45:51 +0200

    Cleaned Code

--- src/com/company/BenutzerTest.java
+++ src/com/company/BenutzerTest.java
@@ -5,8 +5,6 @@ import Sport.Benutzer;
 import Sport.Basketball;
 import Sport.Hindernislauf;
 import Sport.Hochsprung;
-import Sport.Leichtathletik;
-import Sport.Mannschaftssport;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,44 +15,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BenutzerTest {
 
-    private Benutzer TestBenutzer;
-    private Sport Fussball;
-    private Sport Basketball;
-    private Sport Hindernislauf;
-    private Sport Hochsprung;
+    private Benutzer testBenutzer;
+    private Sport fussball;
+    private Sport basketball;
+    private Sport hindernislauf;
+    private Sport hochsprung;
     private List<Sport> sporte;
 
     @BeforeEach
     public void setup(){
-        Fussball = new Fussball();
-        Basketball = new Basketball();
-        Hindernislauf = new Hindernislauf();
-        Hochsprung = new Hochsprung();
+        fussball = new Fussball();
+        basketball = new Basketball();
+        hindernislauf = new Hindernislauf();
+        hochsprung = new Hochsprung();
         sporte = new ArrayList<>();
-        sporte.add(Fussball);
-        sporte.add(Basketball);
-        sporte.add(Hindernislauf);
-        sporte.add(Hochsprung);
-        TestBenutzer = new Benutzer("Andrei", "Popa", sporte);
+        sporte.add(fussball);
+        sporte.add(basketball);
+        sporte.add(hindernislauf);
+        sporte.add(hochsprung);
+        testBenutzer = new Benutzer("Andrei", "Popa", sporte);
     }
 
     @Test
     public void kalkuliereZeit(){
-        assertEquals(TestBenutzer.kalkuliereZeit(), 170);
+        assertEquals(testBenutzer.kalkuliereZeit(), 170);
     }
 
     @Test
     public void testKalkuliereZeit(){
-        assertEquals(TestBenutzer.kalkuliereZeit(Fussball), 65);
-        assertEquals(TestBenutzer.kalkuliereZeit(Basketball), 55);
-        assertEquals(TestBenutzer.kalkuliereZeit(Hindernislauf), 30);
-        assertEquals(TestBenutzer.kalkuliereZeit(Hochsprung), 20);
+        assertEquals(testBenutzer.kalkuliereZeit(fussball), 65);
+        assertEquals(testBenutzer.kalkuliereZeit(basketball), 55);
+        assertEquals(testBenutzer.kalkuliereZeit(hindernislauf), 30);
+        assertEquals(testBenutzer.kalkuliereZeit(hochsprung), 20);
 
     }
     
     @Test
     public void testKalkuliereDurschnittszeit(){
-        assertEquals(TestBenutzer.kalkuliereDurschnittszeit(), 42.5);
+        assertEquals(testBenutzer.kalkuliereDurschnittszeit(), 42.5);
     }
 
     public void testAll(){
